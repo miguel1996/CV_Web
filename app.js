1f7a40957f26886ca79704be58b70577b89de17b
miguel1996 <34025674+miguel1996@users.noreply.github.com>
2020-10-15 14:52:45 +0100

    Cover Section
    Static HTML + CSS page with introductory info

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname+'/public/index.html')
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('', (req, res) => {
+  //res.sendFile(__dirname+'/views/index.html')
+  res.render('index')
   console.log('Connection established with client')
 })
+
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
 })
+
+app.use(express.static('public'))
