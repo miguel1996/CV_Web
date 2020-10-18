bab373a62ced7fd3b381947c9e9f21803ce49a37
miguel1996 <34025674+miguel1996@users.noreply.github.com>
2020-10-18 19:57:16 +0100

    About Section
    Added About section, personal info in JSON, CV in Gitgraph

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const app = express()
 const port = 3000
 
 app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/img', express.static(__dirname + 'public/img'))
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
@@ -20,5 +17,3 @@ app.get('', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
 })
-
-app.use(express.static('public'))
