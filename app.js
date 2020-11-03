be848b860137cd6a9b2d8aea6892bcb5863d46ce
miguel1996 <34025674+miguel1996@users.noreply.github.com>
2020-11-03 15:15:08 +0000

    JSON database
    Created JSON database with personal data, which is retrieved by view, to facilitate modifiability and scalability.

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const db = require('./data')
+
 app.use(express.static('public'))
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.get('', (req, res) => {
-  //res.sendFile(__dirname+'/views/index.html')
-  res.render('index')
+  res.render('index', db.data)
   console.log('Connection established with client')
 })
 
---
+++ data.js
@@ -0,0 +1,89 @@
+const data = {
+	data: {
+		name: "Miguel Andrade",
+		career: "Software Developer",
+		spoken_languages: ["Portuguese", "English"],
+		skills: ["Git", "MySQL", "HTML5/CSS3", "Javascript/JQuery", "Node.JS", "PHP", "Laravel", "Python", "Django", "C#", "Unity", "Java", "Android Studio", "Processing"],
+		about: "Hi, I'm Miguel, a Computer Engineering graduate mostly interested in Web-based applications, currently based in Portugal. <br><br> I'm a versatile, determined person, continuously acquiring new knowledge, to evolve both personally and professionally. I'm always eager for a challenge, and I'm never afraid to use the right tools for the job, regardless of my experience with them.<br><br> Currently, I'm working as an Assistant Researcher in ITI/LARSyS, feel free to contact me through the form bellow, or any of my social media."
+		cv_file: "#",
+		timeline: [
+			bachelor: {
+				title: "Computer Engineering Bachelor's",
+				date: "15/09/2014",
+				description: ""
+			},
+			union: {
+				title: "Volunteering in Student's Union",
+				date: "21/10/2014",
+				description: ""
+			},
+			graduation_bachelor: {
+				title: "Graduation from Bachelor's",
+				date: "04/06/2018",
+				description: ""
+			},
+			end_union: {
+				title: "Concluded Volunteering in Student's Union",
+				date: "13/06/2018",
+				description: ""
+			},
+			internship: {
+				title: "Web Developer in Internship Program",
+				date: "01/08/2018",
+				description: ""
+			},
+			end_internship: {
+				title: "End of Internship",
+				date: "31/08/2018",
+				description: ""
+			},
+			master: {
+				title: "Computer Engineering Master's",
+				date: "17/09/2018",
+				description: ""
+			},
+			thesis: {
+				title: "Initiated Master Thesis",
+				date: "16/09/2019",
+				description: ""
+			},
+			researcher: {
+				title: "Assistant Researcher in ITI/LARSyS",
+				date: "01/01/2020",
+				description: ""
+			}
+		],
+
+		portfolio: [
+			ardome: {
+				title: "ARDome",
+				description: "ARDome is a persuasive interface for inspiring pro-environmental behaviors, by leveraging Augmented Reality technology. This app was built through Unity and ARCore.",
+				github: "",
+				download: "",
+				img: ""
+			},
+			easyEvent: {
+				title: "easyEvent",
+				description: "easyEvent is an event scheduling platform on which the user is capable of creating and submiting custom forms. This is a group project from a lecture developed through Laravel and MySQL.",
+				github: "",
+				download: "",
+				img: ""
+			},
+			cv: {
+				title: "Personal Website",
+				description: "This very own website was developed as a means to promote what I do best. It's a simple web page powered by a Node.JS server, coupled with Express and Embed.JS.",
+				github: "",
+				download: "",
+				img: ""
+			}
+		],
+
+		contact: {
+			email: "",
+			linkedin: "",
+			github: ""
+		}
+	}
+}
+
+module.exports.data = data;
