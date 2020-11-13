bbb6770d6844defd01845281ba5b36d61c44b46c
miguel1996 <34025674+miguel1996@users.noreply.github.com>
2020-11-13 17:07:48 +0000

    Mouse tracker

--- public/js/main.js
+++ public/js/main.js
@@ -79,5 +79,32 @@ $(document).ready(function(){
 		obj.html(html);
 	}
 	setInterval(animateDots.bind(null, loading_dots), 500);	
+
+
+	function getMousePosition(){
+		document.onmousemove = handleMouseMove;
+	    function handleMouseMove(event) {
+	        var eventDoc, doc, body;
+
+	        event = event || window.event; 
+
+	        if (event.pageX == null && event.clientX != null) {
+	            eventDoc = (event.target && event.target.ownerDocument) || document;
+	            doc = eventDoc.documentElement;
+	            body = eventDoc.body;
+
+	            event.pageX = event.clientX +
+	              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+	              (doc && doc.clientLeft || body && body.clientLeft || 0);
+	            event.pageY = event.clientY +
+	              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+	              (doc && doc.clientTop  || body && body.clientTop  || 0 );
+	        }
+
+	        console.log(event.pageX + " " + event.pageY);
+	    }
+	}
+	getMousePosition();
+
 });
 
